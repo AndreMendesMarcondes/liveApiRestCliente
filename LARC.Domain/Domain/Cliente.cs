0f926939a2962174390d0bf4e2395fa30730859d
André Mendes <andre.marcondes@predify.me>
2023-04-05 20:18:18 -0300

    add datanotations

--- LARC.Domain/Domain/Cliente.cs
+++ LARC.Domain/Domain/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace LARC.Domain.Domain
 {
@@ -8,8 +9,18 @@ namespace LARC.Domain.Domain
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; private set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]        
+        [MaxLength(20,ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres")]        
+        [MinLength(2,ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres")]        
         public string Nome { get; set; }
+
+        [Range(18, 999)]
         public int Idade { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [MaxLength(11, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres")]
+        [MinLength(11, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres")]
         public string CPF { get; set; }
     }
 }
